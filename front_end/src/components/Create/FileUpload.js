5643678837501086ec440db886e49b9b7d097970
bluer <54896552+1046069586@users.noreply.github.com>
2022-10-24 10:32:37 +0800

    Update FileUpload.js

--- front_end/src/components/Create/FileUpload.js
+++ front_end/src/components/Create/FileUpload.js
@@ -3,8 +3,8 @@ import { message, Upload } from 'antd';
 
 const { Dragger } = Upload;
 
-const key = '7a333580d9d43ad228fc';
-const secret = 'da0380b0a2894b36f73fa8f0c231df8f31319efcbdd9d3d2837666e37048bbfa';
+const key = 'your key';
+const secret = 'your secret';
 
 const props = {
   name: 'file',
